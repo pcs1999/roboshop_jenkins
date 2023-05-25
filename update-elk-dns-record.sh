d9821266d13b5e300358b259a58fe773bf093e7b
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-25 12:37:34 +0530

    changed to new account jenkins server

---
+++ update-elk-dns-record.sh
@@ -0,0 +1,17 @@
+IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=elastic"  --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+
+echo '
+{
+  "Comment": "CREATE/DELETE/UPSERT a record ",
+  "Changes": [{
+    "Action": "UPSERT",
+    "ResourceRecordSet": {
+      "Name": "elastic.chandupcs.online",
+      "Type": "A",
+      "TTL": 15,
+      "ResourceRecords": [{ "Value": "IPADDRESS"}]
+    }}]
+}' | sed -e "s/IPADDRESS/${IP}/" >/tmp/elastic.json
+
+ZONE_ID="Z04623222IZKOJZ0BZ3PB"
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/elastic.json | jq .
