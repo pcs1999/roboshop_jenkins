707aa7d4cad66d8f116d1fc4f4c24ae6dc811ac0
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-25 11:10:53 +0530

    changed to new account jenkins server

--- update-ELK-dns-record.sh
+++ update-ELK-dns-record.sh
@@ -4,14 +4,14 @@ echo '
 {
   "Comment": "CREATE/DELETE/UPSERT a record ",
   "Changes": [{
-    "Action": "CREATE",
+    "Action": "UPSERT",
     "ResourceRecordSet": {
       "Name": "elk.chandupcs.online",
       "Type": "A",
       "TTL": 15,
       "ResourceRecords": [{ "Value": "IPADDRESS"}]
     }}]
-}' | sed -e "s/IPADDRESS/${IP}/" >/tmp/ELK.json
+}' | sed -e "s/IPADDRESS/${IP}/" >/tmp/elk.json
 
 ZONE_ID="Z04623222IZKOJZ0BZ3PB"
-aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/ELK.json | jq .
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/elk.json | jq .
